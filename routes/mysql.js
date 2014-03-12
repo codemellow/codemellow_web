5e66bfb66bd3d34f81e446305d960d6a2d17b6c0
bubae <widianpear@gmail.com>
2014-03-12 04:58:09 -0700

    bbu mysql

---
+++ routes/mysql.js
@@ -0,0 +1,9 @@
+
+var mysql = require('mysql');
+ var dbconn = mysql.createConnection({
+    host     : 'localhost', // 3306
+    user     : 'root',
+    password : 'ehowlsus14'
+});
+dbconn.connect();
+
