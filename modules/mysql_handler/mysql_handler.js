48dc84ff1a7745cfc20118b81499dce93da1c1d4
root <root@ubuntu.(none)>
2014-03-21 23:07:50 +0900

    db handler setting change

--- modules/mysql_handler/mysql_handler.js
+++ modules/mysql_handler/mysql_handler.js
@@ -8,14 +8,16 @@ switch(process.env.NODE_ENV){
 	dbconn=mysql.createConnection({
 	    host     : 'localhost', // 3306
 	    user     : 'root',
-	    password : 'ehowlsus14'
+	    password : 'ehowlsus14',
+	    database : 'project'
 	});
   	break;
   case 'pro':
     dbconn=mysql.createConnection({
 	    host     : 'database.codemellow.net', // 3306
 	    user     : 'root',
-	    password : 'ehowlsus14'
+	    password : 'ehowlsus14',
+	    database : 'project'
 	});
   	break;
 
@@ -23,14 +25,15 @@ switch(process.env.NODE_ENV){
   	dbconn=mysql.createConnection({
 	    host     : 'database.codemellow.net', // 3306
 	    user     : 'root',
-	    password : 'ehowlsus14'
+	    password : 'ehowlsus14',
+	    database : 'project'
 	});
   	break;
 }
 dbconn.connect();
 
 exports.is_project_exist = function(project_name,callback){
-  dbconn.query("use project");
+  //dbconn.query("use project");
   dbconn.query("select * from project_info where project_name='"+project_name+"'",function(err,rows,cols){
     if(err) throw err;
     if(rows.length==0){
@@ -41,6 +44,6 @@ exports.is_project_exist = function(project_name,callback){
   });
 };
 exports.insert_new_project = function(project_name,username,project_discription,date){
-  dbconn.query("use project");
+  //dbconn.query("use project");
   dbconn.query("insert into project_info (project_name,project_maintatiner,project_discription,create_date) values ('"+project_name+"','"+username+"','"+project_discription+"','"+date+"')")
 };
